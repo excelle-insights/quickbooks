96c594cf0017384d4346ff2156d2a88aefa46705
iammutevu <ianmutevu96@gmail.com>
2026-01-06 00:10:22 +0300

    Fix path

--- scripts/migrate.php
+++ scripts/migrate.php
@@ -2,7 +2,7 @@
 <?php
 declare(strict_types=1);
 
-require dirname(__DIR__, 3) . '/vendor/autoload.php';
+require dirname(__DIR__, 4) . '/vendor/autoload.php';
 use Symfony\Component\Process\Process;
 
 $projectRoot = realpath(dirname(__DIR__, 4));
