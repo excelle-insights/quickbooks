cfbba1041261a011ca4e7df8779dd6a8d51083b9
iammutevu <ianmutevu96@gmail.com>
2026-02-20 01:33:23 +0300

    Update params

--- scripts/create-invoice.php
+++ scripts/create-invoice.php
@@ -10,17 +10,24 @@ $qbo = new QuickBooksManager();
 $result = $qbo->createInvoice([
     'qbo_company_id'  => 1,
     'qbo_customer_id' => 2, // Replace with actual QBO customer ID
-    'invoice_number'  => 'INV-0042',
-    'txn_date'        => '2026-01-22',
-    'due_date'        => '2026-02-05',
+    'invoice_number'  => 'INV-0045',
+    'txn_date'        => '2026-02-22',
+    'due_date'        => '2026-02-25',
     'currency'        => 'KES',
     'items' => [
         [
             'description' => 'Consulting Services',
             'quantity'    => 1,
-            'unit_price'  => 7000,
-            'amount'      => 7000, 
+            'unit_price'  => 7500,
+            'amount'      => 7500, 
             'qbo_class_id' => 1, // Optional: Replace with actual QBO class ID if needed
+        ],
+        [
+            'description' => 'Item 2',
+            'quantity'    => 1,
+            'unit_price'  => 500,
+            'amount'      => 500, 
+            'qbo_class_id' => 2, // Optional: Replace with actual QBO class ID if needed
         ]
     ],
 ]);
--- scripts/get-invoice.php
+++ scripts/get-invoice.php
@@ -3,7 +3,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 use ExcelleInsights\QuickBooks\Facade\QuickBooksManager;
 
-$id = 169;
+$id = 202;
 // Initialize the manager
 $qbo = new QuickBooksManager();
 echo json_encode($qbo->getInvoice($id));
