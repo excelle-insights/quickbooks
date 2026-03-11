096d4e052d7ef745885a4e31e085cedf82c821b9
iammutevu <ianmutevu96@gmail.com>
2026-03-11 15:30:51 +0300

    Fix vendor reference

--- scripts/auth-url.php
+++ scripts/auth-url.php
@@ -1,5 +1,5 @@
 <?php
-require '../../vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 use ExcelleInsights\QuickBooks\Facade\QuickBooksManager;
 use ExcelleInsights\QuickBooks\Controller\OAuthController;
